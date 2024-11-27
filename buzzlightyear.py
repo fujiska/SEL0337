a641d49dcebb8018a84cb5de66457b0009062868
Luana Fujikawa <113643408+fujiska@users.noreply.github.com>
2024-11-27 20:59:40 -0300

    buzzlightyear.py comentado

--- buzzlightyear.py
+++ buzzlightyear.py
@@ -1,11 +1,13 @@
+# O código faz o buzzer apitar uma única vez
 from gpiozero import Buzzer
 from time import sleep
 
 buzzer = Buzzer(17)
-buzz = True
-while buzz:
-	buzzer.on()
-	sleep(1)
-	buzzer.off()
-	sleep(1)
-	buzz = False
+buzz = True # Variável de controle para executar o loop
+
+while buzz: # Loop que será executado enquanto "buzz" for True
+	buzzer.on()  # Ativa o buzzer (emite som)
+	sleep(1)     # Aguarda 1 segundo com o som ligado
+	buzzer.off() # Desativa o buzzer
+	sleep(1)     # Aguarda 1 segundo com o som desligado
+	buzz = False # Define "buzz" como False para encerrar o loop
