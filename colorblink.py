d035a22d62acc26ab82d109ad5423e9ffb39fd78
Luana Fujikawa <113643408+fujiska@users.noreply.github.com>
2024-11-27 20:53:29 -0300

    colorblink.py comentado

--- colorblink.py
+++ colorblink.py
@@ -5,12 +5,12 @@ from time import sleep
 led = RGBLED(red = 9, green = 10, blue = 11)
 
 def fade_to_color(start_color, end_color, steps=50, delay=0.05):
-    """
-    Função que faz uma transição suave (fade) entre duas cores no LED RGB
-    - start_color e end_color: cor inicial/final como uma tupla (r, g, b), onde cada valor está entre 0 e 1
-    - steps: número de passos para a transição (quanto maior, mais suave)
-    - delay: tempo (em segundos) entre cada passo, controlando a velocidade da transição
-    """
+
+    # Função que faz uma transição suave (fade) entre duas cores no LED RGB
+    # - start_color e end_color: cor inicial/final como uma tupla (r, g, b), onde cada valor está entre 0 e 1
+    # - steps: número de passos para a transição (quanto maior, mais suave)
+    # - delay: tempo (em segundos) entre cada passo, controlando a velocidade da transição
+    
 	for step in range(steps+1): # Loop para calcular e aplicar as cores em cada passo
 		# Calcula os valores intermediários de vermelho, verde e azul usando interpolação linear
 		r = start_color[0] + (end_color[0] - start_color[0]) * step/steps
